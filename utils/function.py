4363fe185fa995810b9b8d24b8547b309cdaa2cc
Ruiqi_ZHUANG <76764039+Ricky-ZHUANG@users.noreply.github.com>
2023-12-05 16:01:29 +0800

    Delete utils directory

--- utils/function.py
+++
@@ -1,8 +0,0 @@
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-
-def get_noise_map(img):
-    blur = cv2.bilateralFilter(img,9,75,75)
-    noise_map = img-blur
-    return noise_map
